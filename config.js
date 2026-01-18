73f08d81902ca7c244fcd96a740554087db7a137
namamamksksk-rgb <namamamksksk@gmail.com>
2026-01-19 01:38:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "7F8EDZiR#9DNP7cMb2doaDI48QMQX1T9J3-ZaALOtkVyn72nFyBo",
-  OWNER_NUM: process.env.OWNER_NUM || "94712849964
+  OWNER_NUM: process.env.OWNER_NUM || "94712849964",
   OWNER_NAME: process.env.OWNER_NAME || "Chandani",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
