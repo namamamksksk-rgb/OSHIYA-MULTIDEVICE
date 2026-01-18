7ce4ac1eca9f34d1bb61eaf3f5b721a7fe4ca8ec
namamamksksk-rgb <namamamksksk@gmail.com>
2026-01-19 01:35:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "OdVzHRwD#mve6j6omJ91rEQzYbqd3BESxtTJt0-KEXZWsmPj6FgU",
-  OWNER_NUM: process.env.OWNER_NUM || "94756599952",
-  OWNER_NAME: process.env.OWNER_NAME || "OSHIYA",
+  SESSION_ID: process.env.SESSION_ID || "7F8EDZiR#9DNP7cMb2doaDI48QMQX1T9J3-ZaALOtkVyn72nFyBo",
+  OWNER_NUM: process.env.OWNER_NUM || "94712849964
+  OWNER_NAME: process.env.OWNER_NAME || "Chandani",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE: process.env.MODE || "public",
